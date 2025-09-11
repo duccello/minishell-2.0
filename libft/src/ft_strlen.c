d3758aab88bf8a5e8dcb06368abd8297114e4494
Simone Gasparini <sgaspari@c3a2c7.42berlin.de>
2025-09-11 18:24:14 +0200

    Update libft library

--- libft/src/ft_strlen.c
+++ libft/src/ft_strlen.c
@@ -6,7 +6,7 @@
 /*   By: sgaspari <sgaspari@student.42berlin.d      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/19 13:55:55 by sgaspari          #+#    #+#             */
-/*   Updated: 2025/05/23 13:02:45 by sgaspari         ###   ########.fr       */
+/*   Updated: 2025/09/11 18:10:41 by sgaspari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@ size_t	ft_strlen(const char *s)
 {
 	size_t	len;
 
+	if (s == NULL)
+		return (0);
 	len = 0;
 	while (s[len])
 		len++;
