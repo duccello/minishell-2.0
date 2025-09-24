5b1a900fd3b29d9aae3c9969796682dc00c89dba
Dante Uccello <duccello@c3a2c5.42berlin.de>
2025-09-24 13:36:35 +0200

    Export handles multiple exports, with the right return value. Unset return value corrected. And edge cases handles in both

--- src/export.c
+++ src/export.c
@@ -10,28 +10,64 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+
+#include "built_in.h"
 #include "cmds.h"
 #include "data.h"
-#include "libft.h"
+#include "ft_fprintf.h"
 #include "list.h"
+#include "libft.h"
 #include <stdbool.h>
 #include <stdio.h>
+#include <unistd.h>
 
+static bool	is_parameter_name_invalid(char *s);
 static bool	is_var_dup(char *s, t_node *list);
 static int	find_equal(char *s);
 
 void	ft_export(t_cmd *p)
 {
 	t_node	*node;
+	size_t	i;
 
 	if (p->argv[1] == NULL)
-		return ;
-	if (ft_strchr(p->argv[1], '=') == NULL)
-		return ;
-	if (is_var_dup(p->argv[1], p->data->envp) == true)
-		delete_node(&p->data->envp, p->argv[1], find_equal(p->argv[1]));
-	node = create_node(p->argv[1]);
-	append_node(&p->data->envp, node);
+		env(p);
+	i = 1;
+	while (p->argv[i] != NULL)
+	{
+		if (is_parameter_name_invalid(p->argv[i]) == true)
+		{
+			ft_fprintf(STDERR_FILENO,
+				"export: %s: invalid parameter name\n", p->argv[i]);
+			p->data->ret_val = 1;
+		}
+		else if (ft_strchr(p->argv[i], '=') == NULL)
+			;
+		else
+		{
+			if (is_var_dup(p->argv[i], p->data->envp) == true)
+				delete_node(&p->data->envp, p->argv[i], find_equal(p->argv[i]));
+			node = create_node(p->argv[i]);
+			append_node(&p->data->envp, node);
+		}
+		i++;
+	}
+}
+
+static bool	is_parameter_name_invalid(char *s)
+{
+	size_t	i;
+
+	if (s[0] == '=' || ft_isdigit(s[0]))
+		return (true);
+	i = 0;
+	while (s[i] != '=' && s[i] != '\0')
+	{
+		if (!(ft_isalnum(s[i]) || s[i] == '_'))
+			return (true);
+		i++;
+	}
+	return (false);
 }
 
 static bool	is_var_dup(char *s, t_node *list)
