2a578dd1ed004d6fb9852a60a33f81f08d5aaf38
Dante Uccello <duccello@c3a2c5.42berlin.de>
2025-09-25 19:12:23 +0200

    grammar

--- src/data.c
+++ src/data.c
@@ -19,9 +19,9 @@
 #include <stdbool.h>
 #include <stdlib.h>
 
-static void	modify_shell_lvl(t_data *data);
-static void	increse_level(t_data *data, t_node *curr, char *key);
 static void	modify_shell_name(t_data *data);
+static void	modify_shell_lvl(t_data *data);
+static void	increase_level(t_data *data, t_node *curr, char *key);
 
 t_data	*create_data(char **envp)
 {
@@ -36,41 +36,6 @@ t_data	*create_data(char **envp)
 	modify_shell_name(data);
 	modify_shell_lvl(data);
 	return (data);
-}
-
-static void	modify_shell_lvl(t_data *data)
-{
-	t_node	*curr;
-
-	curr = data->envp;
-	while (curr != NULL)
-	{
-		if (ft_strncmp(curr->s, "SHLVL=", ft_strlen("SHLVL=")) == 0)
-		{
-			increse_level(data, curr, "SHLVL=");
-			return ;
-		}
-		curr = curr->next;
-	}
-}
-
-static void	increse_level(t_data *data, t_node *curr, char *key)
-{
-	char	*new_level;
-	char	*new_shlvl;
-	size_t	level;
-	t_node	*node;
-
-	node = NULL;
-	level = ft_atoi(curr->s + 6);
-	level++;
-	new_level = ft_itoa(level);
-	new_shlvl = ft_strjoin(key, new_level);
-	free(new_level);
-	delete_node(&data->envp, key, ft_strlen(key));
-	node = create_node(new_shlvl);
-	free(new_shlvl);
-	append_node(&data->envp, node);
 }
 
 static void	modify_shell_name(t_data *data)
@@ -91,4 +56,39 @@ static void	modify_shell_name(t_data *data)
 		}
 		curr = curr->next;
 	}
+}
+
+static void	modify_shell_lvl(t_data *data)
+{
+	t_node	*curr;
+
+	curr = data->envp;
+	while (curr != NULL)
+	{
+		if (ft_strncmp(curr->s, "SHLVL=", ft_strlen("SHLVL=")) == 0)
+		{
+			increase_level(data, curr, "SHLVL=");
+			return ;
+		}
+		curr = curr->next;
+	}
+}
+
+static void	increase_level(t_data *data, t_node *curr, char *key)
+{
+	char	*new_level;
+	char	*new_shlvl;
+	size_t	level;
+	t_node	*node;
+
+	node = NULL;
+	level = ft_atoi(curr->s + 6);
+	level++;
+	new_level = ft_itoa(level);
+	new_shlvl = ft_strjoin(key, new_level);
+	free(new_level);
+	delete_node(&data->envp, key, ft_strlen(key));
+	node = create_node(new_shlvl);
+	free(new_shlvl);
+	append_node(&data->envp, node);
 }
